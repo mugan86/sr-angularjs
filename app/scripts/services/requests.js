2ae3ab97c8f42173f47441776d313137f96ffd3a
anartzmugika <mugan86@gmail.com>
2017-01-18 19:55:20 +0100

    Create request service to make all server request (get or post). pending to add new functionalities and variants

---
+++ app/scripts/services/requests.js
@@ -0,0 +1,66 @@
+'use strict';
+
+/* jshint unused: vars */
+/*function makeGetRequest(http, q, url)
+{
+  
+  return http.get(url).then(function(response) {
+            if (typeof response.data === 'object') {
+              console.log(response.data);
+                return response.data;
+            } else {
+                // invalid response
+                return q.reject(response.data);
+            }
+
+            }, function(response) {
+                // something went wrong
+                return q.reject(response.data);
+            });
+}*/
+
+angular.module('serviraceApp')
+  .service('requestService', function requestService($http, $q) {
+    return {
+      GetRequest: function(url) 
+      {
+
+        console.log('Make GET REQUEST(*): ' + url);
+
+        return $http.post(url).then(function(response) {
+            if (typeof response.data === 'object') {
+              console.log(response.data);
+                return response.data;
+            } else {
+                // invalid response
+                return $q.reject(response.data);
+            }
+
+            }, function(response) {
+                // something went wrong
+                return $q.reject(response.data);
+            });
+      },
+
+      PostRequest: function(url) 
+      {
+
+        console.log('Make POST REQUEST(*): ' + url);
+
+        return $http.post(url).then(function(response) {
+            if (typeof response.data === 'object') {
+              console.log(response.data);
+                return response.data;
+            } else {
+                // invalid response
+                return $q.reject(response.data);
+            }
+
+            }, function(response) {
+                // something went wrong
+                return $q.reject(response.data);
+            });
+      },
+
+    };
+  });

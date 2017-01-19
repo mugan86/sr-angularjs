a0375f645985f2ff80fb9086ff3a70a1972be9de
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-19 12:58:14 +0100

    Add info about maps

--- app/constants/values.js
+++ app/constants/values.js
@@ -12,3 +12,44 @@ angular.module('serviraceApp')
 .constant('OPENWEATHER_APP_ID', '"&APPID=58dda22dc16b4ec458a95a0a7f2e921d"')
 .constant('YOUTUBE_API_KEY', 'AIzaSyBDMOCm6nX5P1KiotGWinHvBN3sozVjhRQ')
 ;
+
+/*
+public static String getTrackHtmlFromWikiloc (int id)
+	{
+		return "<html>"
+	            + "  <head>"
+	            + "  </head>"
+	            + "  <body>"
+	            + " <iframe frameBorder=\"0\" src=\"http://es.wikiloc.com/wikiloc/spatialArtifacts.do?event=view&id="
+	            + 	id+"&measures=on&title=on&near=off&images=off&maptype=S\" width=\"100%\" height=\"100%\"></iframe>"
+	            + "  </body>"
+	            + "</html>";
+	}
+
+	public static String getTrackHTMLFromIbilbideak (int id)
+	{
+
+        return "<iframe height=\"100%\" style=\"height:100%;width:100%;margin:0;\" frameBorder=\"0\" src=\"http://mugibili.euskadi.eus/mugibili/minimap.do?id="+id+"\"></iframe>"
+        ;
+		/*return "<html>"
+	            + "  <head>"
+	            + "  </head>"
+	            + "  <body>"
+	            + "		<iframe style=\"height:100%;width:100%;margin:0;\" frameBorder=\"0\" "+
+							"src=\"http://ibilbideak.euskadi.net/ibilbideak/minimap.do?id="+id+"\"></iframe>"
+	            +"  </body>"
+			    + "</html>";
+	}
+
+    public static String getTrackHTMLFromGPSies ()
+    {
+        return "<html>"
+                + "  <head>"
+                + "  </head>"
+                + "  <body>"
+                +"          <iframe src=\"http://www.gpsies.com/mapOnly.do?fileId=nzdgcgtzcodionmq\" "+
+                            "width=\"100%\" height=\"100%\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\"></iframe>"
+                +"  </body>"
+                + "</html>";
+    }
+*/

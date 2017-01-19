ddb5cd2784d61858bb35d8d06a78ebcf47e19f47
anartzmugika <mugan86@gmail.com>
2017-01-19 20:57:26 +0100

    Update

--- app/scripts/services/requests.js
+++ app/scripts/services/requests.js
@@ -12,21 +12,23 @@ angular.module('serviraceApp')
             return responseService.Response(response);
             }, function(response) {
                 // something went wrong
+                console.log(response);
                 return $q.reject(response.data);
             });
       },
 
-      PostRequest: function(url) 
+      PostRequest: function(req) 
       {
 
-        console.log('Make POST REQUEST(*): ' + url);
+        console.log('Make POST REQUEST(*): ' + req);
 
-        return $http.post(url).then(function(response) {
+        return $http.post(req).then(function(response) {
             
               return responseService.Response(response);
 
             }, function(response) {
                 // something went wrong
+                console.log(response);
                 return $q.reject(response.data);
             });
       },

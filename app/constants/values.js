e44adaa75193ad1009d0c2bf0cdec4e4b1093a0f
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-19 12:56:18 +0100

    Add different values constants

---
+++ app/constants/values.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name translationsApp.constant:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the translationsApp
+ */
+angular.module('serviraceApp')
+
+.constant('OPENWEATHER_APP_ID', '"&APPID=58dda22dc16b4ec458a95a0a7f2e921d"')
+.constant('YOUTUBE_API_KEY', 'AIzaSyBDMOCm6nX5P1KiotGWinHvBN3sozVjhRQ')
+;

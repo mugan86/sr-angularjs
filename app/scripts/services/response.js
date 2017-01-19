55f84b5972b39cf0f02d7da9d07b26f2ec33d2a9
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-19 08:13:05 +0100

    Manage response and return to request GET or POST

---
+++ app/scripts/services/response.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('serviraceApp')
+.service('responseService', function responseService($q) {
+    return {
+      Response: function(response) 
+      {
+
+        if (typeof response.data === 'object') {
+              console.log(response.data);
+                return response.data;
+        } else {
+            // invalid response
+            return $q.reject(response.data);
+        }
+      },
+    };
+  });

3057f763d50dbb1e14d09e806ad7ce5b155d90ba
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-19 13:26:59 +0100

    Update values constants file

--- app/constants/values.js
+++ app/constants/values.js
@@ -9,7 +9,7 @@
  */
 angular.module('serviraceApp')
 
-.constant('OPENWEATHER_APP_ID', '"&APPID=58dda22dc16b4ec458a95a0a7f2e921d"')
+.constant('OPENWEATHER_APP_ID', '&APPID=58dda22dc16b4ec458a95a0a7f2e921d')
 .constant('YOUTUBE_API_KEY', 'AIzaSyBDMOCm6nX5P1KiotGWinHvBN3sozVjhRQ')
 ;
 

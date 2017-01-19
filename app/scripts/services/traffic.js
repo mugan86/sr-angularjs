cc8b58db5180ec6edbd0915b2e656006c55ce90b
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-19 08:13:42 +0100

    Add traffic service to get traffic incidents info

---
+++ app/scripts/services/traffic.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('serviraceApp')
+  .service('trafficService', function trafficService(LOCALHOST, requestService) {
+    return {
+      getPortsSituation: function() 
+      {
+
+        var url = LOCALHOST+ 'traffic/get/last_incidents.php?ports=1';
+
+        return requestService.PostRequest(url);
+      },
+      getLastTrafficIncidents: function ()
+      {
+        var url = LOCALHOST+ 'traffic/get/last_incidents.php?ports=0';
+      
+
+        return requestService.GetRequest(url);
+
+
+      }
+    };
+});

0e0e9b5b00334af83fd27109d2a3796ad7a0a37d
anartzmugika <mugan86@gmail.com>
2017-01-20 21:18:35 +0100

    Add race services function

--- app/scripts/services/races.js
+++ app/scripts/services/races.js
@@ -10,28 +10,27 @@ angular.module('serviraceApp')
 
         return requestService.PostRequest(url, params);
       },
-      getLastTrafficIncidents: function ()
+      getSelectRaceNearServices: function (code, category, distance, circle)
       {
-        var url = LOCALHOST+ 'traffic/get/last_incidents.php?ports=0';
+        var url = 'http://mugan86.com/serviraces/api/v1/race/get/test_post.php';
       
-
-        return requestService.GetRequest(url);
+        return requestService.PostRequest(url, {race_name: code, category_filt: category, service_distance: distance, circle_circuit: circle});
 
 
       },
-      /*getSelectRacesPhotos: function (params)
+      getSelectChampionshipRaces: function (code)
       {
-        //http://mugan86.com/serviraces/api/v1/
-        return $http.post(LOCALHOST + "race/get/race_photos.php", params).success(
+        var url = 'http://192.168.1.4/api-servirace/api/v2/race/get/cup_races.php';
 
-            function(data) {
+        //Very important to send correctly params with GET
+        var data = { cup_code: code};
 
-              console.log("CONFIG.JS 76 (Register DEVICE!!)");
-              return data;
-            }
-        );
-        //return request.PostRequest(url);
-      }*/
+        var config = {
+          params: data
+        };
+
+        return requestService.GetRequestParams(url, config);
+      },
     };
 });
 

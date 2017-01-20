59ba795c55be85b16140336486ba64e366271039
anartzmugika <mugan86@gmail.com>
2017-01-20 21:16:26 +0100

    Make 3 API request to API in GET and POST variants

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,32 @@
  * Controller of the serviraceApp
  */
 angular.module('serviraceApp')
-  .controller('MainCtrl', function (racesService, trafficService, $http, $q, LOCALHOST , RACES) {
-    
+  .controller('MainCtrl', function (racesService, trafficService) {
+
+
     trafficService.getLastTrafficIncidents()
             .then(function(data) {
         console.log(data.length);
+
+
+        racesService.getSelectChampionshipRaces('goierri_trail_2017')
+            .then(function(data) {
+	        console.log(data.length);
+
+
+	        racesService.getSelectRaceNearServices('aloñako_igoera', 'restaurant', '10', '1')
+	        	.then(function(data) {
+	        	console.log(data.length);
+
+		    }, function(response) {
+		        // something went wrong
+		       console.log(response);
+		    });
+
+	    }, function(response) {
+	        // something went wrong
+	       console.log(response);
+	    });
 
     }, function(response) {
         // something went wrong

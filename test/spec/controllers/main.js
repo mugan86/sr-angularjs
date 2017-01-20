fce131a15578153a634623f5cc211cd1a8a09c8c
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-20 07:24:03 +0100

    Add code to make GET and POST

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -15,9 +15,61 @@ describe('Controller: MainCtrl', function () {
       $scope: scope
       // place here mocked dependencies
     });
+
+
+    //Example
+
+
+    /*var url_post_server_mugan = LOCALHOST + 'race/get/test_post.php';
+                    
+    //POST
+
+    $http.post(url_post_server_mugan, {race_name: 'aloñako_igoera', category_filt: 'restaurant', service_distance: '10', circle_circuit: '1'})
+                .then(function(response) {
+    if (typeof response.data === 'object') {
+      console.log(response);
+      console.log(response.data);
+      console.log(response.data.length);
+        return response.data;
+    } else {
+      console.log(response.data);
+        // invalid response
+        return $q.reject(response.data);
+    }
+
+    }, function(response) {
+      console.log(response);
+        // something went wrong
+        return $q.reject(response.data);
+    });
+
+    //GET
+
+    $http.get('http://192.168.110.131/test.php')
+                .then(function(response) {
+    if (typeof response.data === 'object') {
+      console.log(response);
+      console.log(response.data);
+      console.log(response.data.length);
+        return response.data;
+    } else {
+      console.log(response.data);
+        // invalid response
+        return $q.reject(response.data);
+    }
+
+    }, function(response) {
+      console.log(response);
+        // something went wrong
+        return $q.reject(response.data);
+    });*/
+
+
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
     expect(MainCtrl.awesomeThings.length).toBe(3);
   });
+
+
 });

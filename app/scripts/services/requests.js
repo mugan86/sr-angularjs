98614a3597da14b78046e4fb72f9736da8263daa
anartzmugika <mugan86@gmail.com>
2017-01-20 21:18:10 +0100

    GetRequestParams function add to make GET request with params

--- app/scripts/services/requests.js
+++ app/scripts/services/requests.js
@@ -3,12 +3,12 @@
 angular.module('serviraceApp')
   .service('requestService', function requestService($http, $q, responseService) {
     return {
-      GetRequest: function(url) 
+      GetRequestParams: function(url, params) 
       {
 
-        console.log('Make GET REQUEST(*): ' + url);
+        console.log('Make GET REQUEST(Params): ' + url + ' PArams: ' + angular.toJson(params));
 
-        return $http.post(url).then(function(response) {
+        return $http.get(url, params).then(function(response) {
             return responseService.Response(response);
             }, function(response) {
                 // something went wrong
@@ -17,12 +17,26 @@ angular.module('serviraceApp')
             });
       },
 
-      PostRequest: function(req) 
+      GetRequest: function(url) 
       {
 
-        console.log('Make POST REQUEST(*): ' + req);
+        console.log('Make GET REQUEST(*): ' + url);
 
-        return $http.post(req).then(function(response) {
+        return $http.get(url).then(function(response) {
+            return responseService.Response(response);
+            }, function(response) {
+                // something went wrong
+                console.log(response);
+                return $q.reject(response.data);
+            });
+      },
+
+      PostRequest: function(url, params) 
+      {
+
+        console.log('Make POST REQUEST(*): ' + url + '   ' + params);
+
+        return $http.post(url, params).then(function(response) {
             
               return responseService.Response(response);
 

e2e90ebb1766912f414cbc421f19eb808b19d5ff
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-23 11:33:03 +0100

    Add Youtube services to load select videos and info from Youtube

---
+++ app/scripts/services/youtube.js
@@ -0,0 +1,91 @@
+'use strict';
+
+angular.module('serviraceApp')
+
+
+.service('youtubeServices', function youtubeServices($http, YOUTUBE, requestService) {
+
+    return {
+      
+      getPlayLists: function(items) {
+
+        var url = YOUTUBE.url + "playlists?part=snippet" + YOUTUBE.channelId + "&maxResults=" + items + YOUTUBE.apiKey;
+
+        console.log(url);
+
+        return $http.get(url).success(
+
+            function(resp)
+            {
+              //console.log(resp);
+              return resp;
+            }
+        );
+      },
+
+      getSelectPlayListVideos: function(id, items) {
+
+        var url = YOUTUBE.url + "playlistItems?part=snippet" + "&playlistId=" + id + "&maxResults=" + items + YOUTUBE.apiKey;
+
+        //https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLFjjfF0vHULYRlHnw8MCr3L4KUAGktyQ4&key=AIzaSyDjSQp1NtdHgtAZT2_3gQADTnmpeppYdk8
+
+        console.log(url);
+
+        return $http.get(url).success(
+
+            function(resp)
+            {
+              //console.log(resp);
+              return resp;
+            }
+        );
+      },
+
+      getUploadVideos: function() {
+
+        var local = 'files/blogak.json';
+
+        return $http.get(local).success(
+
+            function(resp)
+            {
+              //console.log(resp);
+              return resp;
+            }
+        );
+      },
+
+      getLastNumberVideos: function (items, channel) {
+      	var channelId = YOUTUBE.runneaChannelId;
+		if (channel > 1)
+		{
+			channelId = YOUTUBE.mugan86ChannelId;
+		}
+        var url = YOUTUBE.url + "search?part=snippet" + channelId + "&maxResults=" + items + "&order=date" + YOUTUBE.apiKey;
+        console.log(url);
+
+        //https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCTh7-deUJBNv2tHRiMGcXxg&maxResults=50&order=date&key=AIzaSyDjSQp1NtdHgtAZT2_3gQADTnmpeppYdk8
+
+        return requestService.GetRequest(url);
+
+        
+      },
+
+      getSelectVideo: function(id) {
+
+        var url = YOUTUBE.url + "videos?part=snippet,contentDetails,statistics,status&id=" + id + YOUTUBE.apiKey;
+        //https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics,status&id=ouDu6REA1ZU&key=AIzaSyDjSQp1NtdHgtAZT2_3gQADTnmpeppYdk8
+        console.log(url);
+        return $http.get(url).success(
+
+            function(resp)
+            {
+              //console.log(resp);
+              return resp;
+            }
+        );
+      },
+
+      //gehiago gehitu daitezke...
+    };
+  });

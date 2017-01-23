cb90ef5bd06405a281392ce604598e221786a3b4
anartzmugika <mugan86@gmail.com>
2017-01-23 20:06:53 +0100

    Add contact controller

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name serviraceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the serviraceApp
+ */
+angular.module('serviraceApp')
+  .controller('ContactCtrl', function () {
+
+
+});

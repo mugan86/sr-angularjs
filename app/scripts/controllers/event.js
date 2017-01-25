e4e54f95e1056f81917645b43ed935d0643b9881
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-25 12:03:13 +0100

    Add Event controller script

---
+++ app/scripts/controllers/event.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name serviraceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the serviraceApp
+ */
+angular.module('serviraceApp')
+  .controller('EventCtrl', function ($scope) {
+
+    $scope.start = true;
+
+});

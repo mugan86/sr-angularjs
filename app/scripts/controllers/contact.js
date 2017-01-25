31978c2759513f79922dbd88112fc390546c0512
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-25 08:35:47 +0100

    Update controller

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -9,52 +9,7 @@
  */
 angular.module('serviraceApp')
   .controller('ContactCtrl', function ($scope) {
-$scope.oneAtATime = true;
 
-  $scope.groups = [{
-    title: 'Dynamic Group Header - 1',
-    content: 'Dynamic Group Body - 1'
-  }, {
-    title: 'Dynamic Group Header - 2',
-    content: 'Dynamic Group Body - 2'
-  },  {
-    title: 'Dynamic Group Header - 3',
-    content: 'Dynamic Group Body - 3'
-  }];
-
-
-  $scope.status = {
-    isCustomHeaderOpen: false,
-    isFirstOpen: true,
-    isFirstDisabled: false
-  };
-
-
-  // work permit form
-  $scope.transforms = [{
-    name: "transform",
-    id: 1,
-    wpformfields: [{
-      employee: '',
-      admount: '',
-      open: true
-    }]
-  }];
-
-  $scope.addtransactionForm = function(transform) {
-
-  	for (var i = 0; i < $scope.transforms.wpformfields.length; i++)
-  	{
-  		$scope.transforms.wpformfields[i].open = false;
-  	}
-    $scope.currentnum = transform.wpformfields.length;
-    //alert("hello");
-
-    transform.wpformfields.push({
-      employee: '',
-      amount: '',
-      open: true
-    });
-  };
+    $scope.start = true;
 
 });

b9b51db8dcae0c9191131fccc43888fadcf581da
anartzmugika <mugan86@gmail.com>
2017-01-25 20:17:58 +0100

    Make server request to get info about races

--- app/scripts/services/races.js
+++ app/scripts/services/races.js
@@ -3,16 +3,22 @@
 angular.module('serviraceApp')
   .service('racesService', function racesService(LOCALHOST, RACES, requestService) {
     return {
-      getNextTwoMonthsRacesData: function(params) 
+      getNextTwoMonthsRacesData: function(first_data, second_data, race_name, race_type, district, distance) 
       {
-          
-        var url = LOCALHOST + RACES.afterBefore;
 
-        return requestService.PostRequest(url, params);
+
+        var url = LOCALHOST.v2 +'race/get/race_info_list.php';
+
+        return requestService.PostRequest(url, {first_data: getTodayData(), 
+                                                  second_data: getFutureData(60), 
+                                                  race_name: race_name, 
+                                                  distance: distance, 
+                                                  race_type: race_type, 
+                                                  district: district});
       },
       getSelectRaceNearServices: function (code, category, distance, circle)
       {
-        var url = 'http://mugan86.com/serviraces/api/v1/race/get/test_post.php';
+        var url =  LOCALHOST.v1 + 'race/get/test_post.php';
       
         return requestService.PostRequest(url, {race_name: code, category_filt: category, service_distance: distance, circle_circuit: circle});
 
@@ -20,13 +26,13 @@ angular.module('serviraceApp')
       },
       getActiveChampionships: function ()
       {
-        var url = 'http://192.168.1.4/api-servirace/api/v2/race/get/championship.php';
+        var url = LOCALHOST.v2 + 'race/get/championship.php';
 
         return requestService.GetRequest(url);
       },
       getSelectChampionshipRaces: function (code)
       {
-        var url = 'http://192.168.1.4/api-servirace/api/v2/race/get/cup_races.php';
+        var url = LOCALHOST.v2 + 'race/get/cup_races.php';
 
         //Very important to send correctly params with GET
         var data = { cup_code: code};
@@ -47,6 +53,7 @@ angular.module('serviraceApp')
       **************************************************************************************/
       getLastEvents: function (event, items)
       {
+        
         var url = 'http://192.168.1.4/api-servirace/api/v2/race/get/last_events.php';
 
         //Very important to send correctly params with GET

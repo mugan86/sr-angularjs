fc70ddc7e037c21b25ebe8147958ab2370aa0377
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-26 11:04:15 +0100

    Log receive data in string format

--- app/scripts/services/requests.js
+++ app/scripts/services/requests.js
@@ -9,6 +9,7 @@ angular.module('serviraceApp')
         console.log('Make GET REQUEST(Params): ' + url + ' PArams: ' + angular.toJson(params));
 
         return $http.get(url, params).then(function(response) {
+            console.log(angular.toJson(response));
             return responseService.Response(response);
             }, function(response) {
                 // something went wrong

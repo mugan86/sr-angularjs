a9115728f181eb1c9663171524a8e5e509dd12de
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-27 10:56:25 +0100

    Update

--- app/scripts/controllers/races.js
+++ app/scripts/controllers/races.js
@@ -11,9 +11,16 @@ angular.module('serviraceApp')
   .controller('RaceCtrl', function ($scope, racesService) {
 
     $scope.start = true;
+    $scope.race = [];
 
-    $scope.race = racesService.getRace();
+    var r = [];
+    r.push(racesService.getRace()[0]);
+    $scope.race = r;
+    console.log(r[0].race_name);
+    console.log($scope.race[0].race_name);
 
-    console.log(angular.toJson($scope.race[0]));
+    /*console.log(angular.toJson($scope.race[0]));
+
+    console.log($scope.race[0].race_climb);*/
 
 });

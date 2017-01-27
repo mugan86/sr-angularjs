18b6c691a78f5c92b7cff450816b243e479871e8
anartzmugika <mugan86@gmail.com>
2017-01-27 20:08:27 +0100

    Update with remove unused function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -69,15 +69,6 @@ angular.module('serviraceApp')
        console.log(response);
     });
 
-  racesService.getErgast()
-      .then(function(data) {
-      console.log(data);
-
-
-    }, function(response) {
-        // something went wrong
-       console.log(response);
-    });
 
   $scope.showMessage = function(pos)
   {
